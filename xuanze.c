d184f4e5d1f728515343d45c78c02961d28ea98e
edz <edz@chuxq.local>
2020-02-05 17:03:41 +0800

    xuanzepaixu

---
+++ xuanze.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int i, j, x, a[11];
+    printf("请输入10个数：\n");
+
+    for (i = 1; i < 11; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 1; i <= 9; i++)
+        for (j = i + 1; j <= 10; j++)
+            if (a[i] > a[j]) {
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+            }
+
+    printf("排序后的顺序是：\n");
+
+    for (i = 1; i <= 10; i++) {
+        printf("%5d", a[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
